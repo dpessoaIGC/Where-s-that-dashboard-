96d965448a38d50d119422c98d367037a1752093
dpessoa <delphine.pessoa@unbabel.com>
2024-06-27 17:55:10 +0100

    Add search functionality

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from catalogue import get_dashboard_catalog, create_update_catalog
+from catalogue.create_catalogue import get_dashboard_catalog, create_update_catalog
+from catalogue.search import get_dashboards
 
 if __name__ == "__main__":
     catalog = get_dashboard_catalog()
     create_update_catalog(catalog)
+    get_dashboards("where can I find the top sales?", catalog)
